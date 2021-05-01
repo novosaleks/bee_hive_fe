bef0f43c25d93300b056dfff43ef7bc06ac5a3fc
MrTwisterAnastasia <thanksforthisgifts@gmail.com>
2021-05-02 00:08:08 +0300

    resolve merge conflicts

--- src/App/app.theme.js
+++ src/App/app.theme.js
@@ -1,27 +1,19 @@
 export const theme = {
-    light: {
-        primaryColor: '#f15a29',
-        primaryBgColor: '#f8f5f2',
-<<<<<<< HEAD
-        textColor: '#000',
-        inputColor: '#000',
-=======
-        primaryBgColorPage: '#FFFFFF',
-        primaryBgColorDiv: '#C4C4C4',
-        textColor: '#000',
-        inputColor: '#000',
+        light: {
+                primaryColor: '#f15a29',
+                primaryBgColor: '#f8f5f2',
+                primaryBgColorPage: '#FFFFFF',
+                primaryBgColorDiv: '#C4C4C4',
+                textColor: '#000',
+                inputColor: '#000',
 
->>>>>>> 8e4d0db... First commit profile page
-    },
-    dark: {
-        primaryColor: '#f15a29',
-        primaryBgColor: '#2f4858',
-<<<<<<< HEAD
-=======
-        primaryBgColorPage: '#374955',
-        primaryBgColorDiv: '#9BAEBC',
->>>>>>> 8e4d0db... First commit profile page
-        textColor: '#fff',
-        inputColor: '#fff',
-    },
+        },
+        dark: {
+                primaryColor: '#f15a29',
+                primaryBgColor: '#2f4858',
+                primaryBgColorPage: '#374955',
+                primaryBgColorDiv: '#9BAEBC',
+                textColor: '#fff',
+                inputColor: '#fff',
+        },
 };
