95f2256df2229a83e1dea3c5bc57052a63340531
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-05-05 15:02:10 +0300

    Completed Navbar

--- src/components/postAuthorAndDate/postAuthorAndDate.js
+++ src/components/postAuthorAndDate/postAuthorAndDate.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { HrLine, StyledDiv, StyledText } from '../../common/style/index';
+import {
+    DivLine,
+    StyledDiv,
+    StyledText,
+} from '../../common/style/index';
 
 const PostAuthorAndDate = (props) => {
-   return (
-      <StyledDiv >
-         <StyledDiv align="center" direction="row" content="space-between">
-            <StyledText fontSize={['1.2em', '1.6em']} align="left">
-               {props.name}
-            </StyledText>
-            <StyledText fontSize={['0.6em', '0.7em']} align="right">
-               {props.date}
-            </StyledText>
-         </StyledDiv>
-         <HrLine color="#F15A29" />
-      </StyledDiv>
-   );
-}
+    return (
+        <StyledDiv>
+            <StyledDiv align='center' direction='row' content='space-between'>
+                <StyledText fontSize={['1.2em', '1.6em']} align='left'>
+                    {props.name}
+                </StyledText>
+                <StyledText fontSize={['0.6em', '0.7em']} align='right'>
+                    {props.date}
+                </StyledText>
+            </StyledDiv>
+            <DivLine/>
+        </StyledDiv>
+    );
+};
 
 export default PostAuthorAndDate;
