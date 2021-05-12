80e8883abb95197375280ce6cd123359a5496824
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-05-12 18:12:18 +0300

    Bind tabs to settings page

--- src/containers/general-information-container/general-information-container.jsx
+++ src/containers/general-information-container/general-information-container.jsx
@@ -5,24 +5,27 @@ import SettingsForm from '../../components/settings-form';
 const GeneralInformationContainer = () => {
 
 // TODO dynamic content generation
-    const fieldLabels = [
-        'name',
-        'date of birth',
-        'location',
-        'occupation',
-        'about me',
-        'password',
-    ];
+          const fieldLabels = [
+              {label: 'name', type: 'text'},
+              {label: 'date of birth', type: 'date'},
+              {label: 'location', type: 'text'},
+              {label: 'occupation', type: 'text'},
+              {label: 'about me', type: 'text'},
+              {label: 'password', type: 'password'},
+          ];
 
-    return (
-        <GeneralSettings>
-            {
-                fieldLabels.map(item => {
-                    return <SettingsForm name={item} label={`${item}:`}/>;
-                })
-            }
-        </GeneralSettings>
-    );
-};
+          return (
+              <GeneralSettings>
+                  {
+                      fieldLabels.map(({label, type}) => {
+                          return <SettingsForm key={label} type={type} name={label}
+                              label={`${label}:`}/>;
+                      })
+                  }
+              </GeneralSettings>
+          );
+      }
+;
 
 export default GeneralInformationContainer;
+;
