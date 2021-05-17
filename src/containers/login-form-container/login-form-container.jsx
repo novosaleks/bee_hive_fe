4e909ccf78aacbaef9c0390f17061fa42cc8e399
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-17 18:11:41 +0300

    implement login query, get user data from the server and redirect to home page if success

--- src/containers/login-form-container/login-form-container.jsx
+++ src/containers/login-form-container/login-form-container.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import InputContainer from '../input-container';
 import { useForm } from 'react-hook-form';
 import LoginForm from '../../components/login-form';
+import { useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+
+import { LOGIN } from "../../graphql/auth";
 
 const LoginFormContainer = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
+    const [login, { data }] = useMutation(LOGIN);
+    const history = useHistory();
 
-    const loginHandler = d => {
-        // TODO implement login service
+    useEffect(() => {
+        if (data) {
+            const user = data.login;
+            console.log("data:", user);
+            // TODO implement saving user data in the cache
+            if (user) {
+                history.push("/");
+            } else {
+                console.error("Login failed!")
+            }
+        }
+    }, [data]);
+
+    const loginHandler = async d => {
+        await login({
+            variables: {
+                email: d.email,
+                password: d.password
+            }
+        });
     };
 
     const LoginInput = (props) => {
