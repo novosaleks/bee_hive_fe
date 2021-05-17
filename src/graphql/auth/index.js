3da4495bd35d2db406af7523f8331243d5c8b50f
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-17 16:33:19 +0300

    add GraphQL queries for auth and user

---
+++ src/graphql/auth/index.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      firstName
+      lastName
+      email
+      password
+      occupation
+      location
+      birthDate
+    }
+  }
+`;
+
+export const LOGOUT = gql`
+mutation {
+  logout
+}
+`;
