bc01f71bf75b9f8e8dfa8cab174d3d53f5269fa5
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-17 16:33:19 +0300

    add GraphQL queries for auth and user

---
+++ src/graphql/user/index.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const GET_ALL_USERS = gql`
+    query {
+        getAllUsers {
+            id
+            firstName
+            lastName
+            email
+            password
+        }
+    }
+`;
+
+export const GET_CURRENT_USER = gql`
+    query {
+        me {
+            id
+            firstName
+            lastName
+            email
+            password
+        }
+    }
+`;
+
+export const CREATE_USER = gql`
+    mutation CreateUser(
+        $firstName: String!
+        $lastName: String!
+        $email: String!
+        $password: String!
+        $occupation: String!
+        $location: String!
+        $birthDate: String!
+        $userInfo: String!
+    ) {
+        createUser(
+            firstName: $firstName
+            lastName: $lastName
+            email: $email
+            password: $password
+            occupation: $occupation
+            location: $location
+            birthDate: $birthDate
+            userInfo: $userInfo
+        )
+    }
+`;
