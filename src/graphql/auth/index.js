18faa1dc8c2b009f934ad29f778600f5209fe35c
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-05-21 19:45:02 +0300

    Fixed login implementation

--- src/graphql/auth/index.js
+++ src/graphql/auth/index.js
@@ -2,18 +2,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
     mutation Login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            id
-            firstName
-            lastName
-            email
-            password
-            occupation
-            location
-            birthDate
-            createdAt
-            updatedAt
-        }
+        login(email: $email, password: $password) 
     }
 `;
 
