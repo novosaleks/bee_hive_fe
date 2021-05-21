ae3c943fe58ba91f46b7486e7ed6aa5542846a9d
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-22 00:50:24 +0300

    implement logout

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGOUT } from '../../graphql/auth';
 import { LogoutDiv } from './logout.style';
 
 const Logout = () => {
     const [logout, { data }] = useMutation(LOGOUT);
-    const logoutHandler = () => {
-        //  logout({
-        //     variables: {},
-        // });
+
+    useEffect(() => {
+        if (data) {
+            const success = data.logout;
+            if (success) {
+                window.location.reload();
+            }
+        }
+    }, [data]);
+
+    const logoutHandler = async () => {
+        await logout();
     };
+
     return <LogoutDiv onClick={logoutHandler} />;
 };
 
