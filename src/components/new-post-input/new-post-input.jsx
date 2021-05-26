ff2660fd2bb9a20dc57241b810948c2435ee04ec
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-26 04:35:47 +0300

    add create post functionality

--- src/components/new-post-input/new-post-input.jsx
+++ src/components/new-post-input/new-post-input.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyledDiv, StyledText } from '../../common/style/index';
 import { InputPost, PostButton } from './new-post-input.style';
+import { useMutation } from '@apollo/client';
+import { CREATE_POST } from '../../graphql/post';
 
 const NewPostInput = () => {
+    const [createPost, { data }] = useMutation(CREATE_POST);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (data) {
+            const success = data.createPost;
+            if (success) {
+                console.log('New post has been created!');
+            }
+        }
+    }, [data]);
+
+    const handleClick = async () => {
+        await createPost({
+            variables: {
+                text: inputRef.current.value,
+            },
+        });
+    };
+
     return (
         <StyledDiv width='100%'>
-            <InputPost placeholder='Share your thoughts with the world!!' />
-            <PostButton width={[135, 170]} height={[45, 55]} mt='3%'>
+            <InputPost
+                ref={inputRef}
+                placeholder='Share your thoughts with the world!!'
+            />
+            <PostButton
+                onClick={handleClick}
+                width={[135, 170]}
+                height={[45, 55]}
+                mt='3%'>
                 <StyledText fontSize={['0.7em', '0.9em']}>Post</StyledText>
             </PostButton>
         </StyledDiv>
--- src/graphql/post/index.js
+++ src/graphql/post/index.js
@@ -1,13 +1,34 @@
 import { gql } from '@apollo/client';
 
+export const CREATE_POST = gql`
+    mutation CreatePost($text: String!) {
+        createPost(text: $text)
+    }
+`;
+
 export const UPDATE_POST = gql`
-    mutation updatePost($text: String!) {
-        updatePost(text: $text)
+    mutation UpdatePost($text: String!, $postId: ID!) {
+        updatePost(text: $text, postId: $postId)
     }
 `;
 
 export const REMOVE_POST = gql`
-    mutation removePost($postId: String!) {
+    mutation RemovePost($postId: Int!) {
         removePost(postId: $postId)
+    }
+`;
+
+export const GET_POSTS_BY_AUTHOR_ID = gql`
+    query GetPostsByAuthorId($authorId: ID!) {
+        getPostsByAuthorId(authorId: $authorId) {
+            id
+            text
+            createdAt
+            updatedAt
+            author {
+                firstName
+                lastName
+            }
+        }
     }
 `;
