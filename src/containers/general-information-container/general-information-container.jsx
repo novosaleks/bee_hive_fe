66d1852f9125595affa444af3e3053aa9efdad24
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-05-27 22:26:10 +0300

    implement profile editing

--- src/containers/general-information-container/general-information-container.jsx
+++ src/containers/general-information-container/general-information-container.jsx
@@ -1,31 +1,96 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import SettingsContainer from '../settings-container';
 import SettingsForm from '../../components/settings-form';
+import { useQuery } from '@apollo/client';
+import { GET_CURRENT_USER } from '../../graphql/user';
 
 const GeneralInformationContainer = () => {
-    // TODO dynamic content generation
-    const formFields = [
-        { label: 'name', type: 'text' },
-        { label: 'date of birth', type: 'date' },
-        { label: 'location', type: 'text' },
-        { label: 'occupation', type: 'text' },
-        { label: 'about me', type: 'text' },
-        { label: 'password', type: 'password' },
-    ];
+    const [user, setUser] = useState(null);
+    const [formFields, setFormFields] = useState(null);
+    const { loading, error, data } = useQuery(GET_CURRENT_USER);
+
+    useEffect(() => {
+        if (data) {
+            setUser(data.currentUser);
+        }
+    }, [data]);
+
+    useEffect(() => {
+        if (user) {
+            setFormFields([
+                {
+                    label: 'First name',
+                    name: 'firstName',
+                    type: 'text',
+                    defaultValue: user.firstName,
+                },
+                {
+                    label: 'Last name',
+                    name: 'lastName',
+                    type: 'text',
+                    defaultValue: user.lastName,
+                },
+                {
+                    label: 'Email',
+                    name: 'email',
+                    type: 'email',
+                    defaultValue: user.email,
+                },
+                {
+                    label: 'Password',
+                    name: 'password',
+                    type: 'password',
+                    defaultValue: '',
+                },
+                {
+                    label: 'Occupation',
+                    name: 'occupation',
+                    type: 'text',
+                    defaultValue: user.occupation,
+                },
+                {
+                    label: 'Location',
+                    name: 'location',
+                    type: 'text',
+                    defaultValue: user.location,
+                },
+                {
+                    label: 'Birth date',
+                    name: 'birthDate',
+                    type: 'date',
+                    defaultValue: user.birthDate,
+                },
+                {
+                    label: 'About me',
+                    name: 'userInfo',
+                    type: 'text',
+                    defaultValue: user.userInfo,
+                },
+            ]);
+        }
+    }, [user]);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{`Error: ${error.message}`}</div>;
 
     return (
-        <SettingsContainer title='General information settings'>
-            {formFields.map(({ label, type }) => {
-                return (
-                    <SettingsForm
-                        key={label}
-                        type={type}
-                        name={label}
-                        label={`${label}:`}
-                    />
-                );
-            })}
-        </SettingsContainer>
+        <>
+            {formFields && (
+                <SettingsContainer title='General information settings'>
+                    {formFields.map(({ label, name, type, defaultValue }) => {
+                        return (
+                            <SettingsForm
+                                key={name}
+                                type={type}
+                                name={name}
+                                label={`${label}:`}
+                                defaultValue={defaultValue}
+                            />
+                        );
+                    })}
+                </SettingsContainer>
+            )}
+        </>
     );
 };
 export default GeneralInformationContainer;
