d95edb0777bf89745d7512f0c4cd45b1f14f91d6
MrTwisterAnastasia <thanksforthisgifts@gmail.com>
2021-05-27 18:42:02 +0300

    fixed input

--- src/components/new-post-input/new-post-input.style.js
+++ src/components/new-post-input/new-post-input.style.js
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
-import { Button, Input, media } from '../../common/style/index';
+import { Button, media } from '../../common/style/index';
 import { getValueFromTheme } from '../../common/utils';
 
-export const InputPost = styled(Input)`
+export const InputPost = styled.textarea`
+    position: relative;
+    background: none;
+    width: 100%;
+    min-height: 35px;
+    outline: none;
+    transition: all 0.1s;
     border: 2px solid #f15a29;
     min-height: 65px;
     margin-top: 3%;
     color: ${getValueFromTheme('inputColor')};
 
+    &:focus {
+        border: 2px solid ${getValueFromTheme('focusColor')};
+    }
+
     &::placeholder {
+        color: ${getValueFromTheme('inputColor')};
         opacity: 0.6;
     }
 `;
