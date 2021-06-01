ef38a6eff78b66ba5d76999f941263a87453020b
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-01 19:07:08 +0300

    remove redundant redirect

--- src/containers/routing-container/routes/unauthorized.jsx
+++ src/containers/routing-container/routes/unauthorized.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import {Redirect, Route, Switch} from "react-router-dom";
-import LoginScreen from "../../../pages/login-screen";
-import RegisterScreen from "../../../pages/register-screen";
+import { Route, Switch } from 'react-router-dom';
+import LoginScreen from '../../../pages/login-screen';
+import RegisterScreen from '../../../pages/register-screen';
 
-const Unauthorized = ({user}) => {
+const Unauthorized = ({ user }) => {
     return (
-        !user ?
+        !user && (
             <Switch>
                 <Route path='/login' exact>
-                    <LoginScreen/>
+                    <LoginScreen />
                 </Route>
                 <Route path='/register' exact>
-                    <RegisterScreen/>
+                    <RegisterScreen />
                 </Route>
             </Switch>
-            : <Redirect to='/'/>
-    )
-}
+        )
+    );
+};
 
 export default Unauthorized;
