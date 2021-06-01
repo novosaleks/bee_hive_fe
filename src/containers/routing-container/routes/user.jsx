49b2eb244136b8ebc4935d845becdbed1aecca9b
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-01 19:18:15 +0300

    add a route by user ID and redirect to user ID if route '/'

--- src/containers/routing-container/routes/user.jsx
+++ src/containers/routing-container/routes/user.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import NavbarContainer from '../../navbar-container';
 import ProfileScreen from '../../../pages/profile-screen';
@@ -8,31 +7,34 @@ import SubscriptionScreen from '../../../pages/subscription-screen';
 import MessagesScreen from '../../../pages/messages-screen';
 
 const User = ({ user }) => {
-    return (
-        user ?
-            <Switch>
-                <Route path='/' exact>
-                    <NavbarContainer />
-                    <ProfileScreen />
-                </Route>
-                <Route path='/settings' exact>
-                    <NavbarContainer />
-                    <SettingsScreen />
-                </Route>
-                <Route path='/news' exact>
-                    <NavbarContainer />
-                    <NewsScreen />
-                </Route>
-                <Route path='/subscription' exact>
-                    <NavbarContainer />
-                    <SubscriptionScreen />
-                </Route>
-                <Route path='/messages' exact>
-                    <NavbarContainer />
-                    <MessagesScreen />
-                </Route>
-            </Switch>
-            : <Redirect to='/login' />
+    return user ? (
+        <Switch>
+            <Route path='/' exact>
+                <Redirect to={`/${user.id}`} />
+            </Route>
+            <Route path='/:userId'>
+                <NavbarContainer />
+                <ProfileScreen />
+            </Route>
+            <Route path='/settings' exact>
+                <NavbarContainer />
+                <SettingsScreen />
+            </Route>
+            <Route path='/news' exact>
+                <NavbarContainer />
+                <NewsScreen />
+            </Route>
+            <Route path='/subscription' exact>
+                <NavbarContainer />
+                <SubscriptionScreen />
+            </Route>
+            <Route path='/messages' exact>
+                <NavbarContainer />
+                <MessagesScreen />
+            </Route>
+        </Switch>
+    ) : (
+        <Redirect to='/login' />
     );
 };
 
