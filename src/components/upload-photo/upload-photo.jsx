ee521feae494ded615568e5a792b97d8ce5915bb
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-03 19:29:32 +0300

    fix upload-photo

--- src/components/upload-photo/upload-photo.jsx
+++ src/components/upload-photo/upload-photo.jsx
@@ -1,34 +1,18 @@
 import { useMutation } from '@apollo/client';
-import { useState } from 'react';
 import { UPLOAD_PHOTO } from '../../graphql/photo';
 
 const UploadPhoto = () => {
-    const [uploadPhoto, { data }] = useMutation(UPLOAD_PHOTO);
+    const [uploadPhoto] = useMutation(UPLOAD_PHOTO, {
+        // TODO add a notification
+        onCompleted: data => console.log(data),
+    });
 
-    useState(() => {
-        if (data) {
-            if (data.uploadPhoto.success) {
-                // TODO add a notification
-                console.log(data.uploadPhoto.message);
-            }
-        }
-    }, [data]);
-
-    // const handleUploadPhoto = ({
-    //     target: {
-    //         validity,
-    //         files: [file],
-    //     },
-    // }) => validity.valid && uploadPhoto({ variables: { file, avatar: true } });
-
-    const handleUploadPhoto = e => {
-        console.log('LOOOOOOOOOOOOOOOOOOOOOOOOOOOOL');
-        const file = e.target.files[0];
-        console.log('FILE:', file);
-        // if (validity.valid) {
-        uploadPhoto({ variables: { file } });
-        // }
-    };
+    const handleUploadPhoto = ({
+        target: {
+            validity,
+            files: [file],
+        },
+    }) => validity.valid && uploadPhoto({ variables: { file, avatar: true } });
 
     return (
         <div>
