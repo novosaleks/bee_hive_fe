926e4c2abbba1b50cc578cdb6620cd64b902c2ab
MrTwisterAnastasia <thanksforthisgifts@gmail.com>
2021-06-03 15:38:52 +0300

    added follow button to others users profilies

--- src/containers/sidebar-user-info-container/sidebar-user-info-container.jsx
+++ src/containers/sidebar-user-info-container/sidebar-user-info-container.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react';
+
 import UserAvatar from '../../components/user-avatar';
 import UserInfo from '../../components/user-info';
 import UserExtraInfo from '../../components/user-extra-info';
 import FollowButton from '../../components/follow-button';
 
+import { useQuery } from '@apollo/client';
+import { GET_CURRENT_USER } from '../../graphql/user';
+
 import { StyledSidebarDiv } from './sidebar-user-info-container.style';
 import { DivLine, StyledText } from '../../common/style/index';
 
 const SideBarUserInfo = ({ user }) => {
+    const { loading, error, data: userData } = useQuery(GET_CURRENT_USER);
+    const [currentUserId, setCurrentUserId] = useState(null);
+    useEffect(() => {
+        if (userData) {
+            setCurrentUserId(userData.currentUser.id);
+        }
+    }, [userData]);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{`Error! ${error.message}`}</div>;
     return (
         <StyledSidebarDiv>
             <UserAvatar
@@ -21,7 +36,7 @@ const SideBarUserInfo = ({ user }) => {
             <StyledText color={'#5dac38'}>{'Drink coffee'}</StyledText>
             <DivLine />
             <UserInfo user={user} />
-            {!user && <FollowButton />}
+            {currentUserId !== user.id && <FollowButton />}
             <UserExtraInfo user={user} />
         </StyledSidebarDiv>
     );
