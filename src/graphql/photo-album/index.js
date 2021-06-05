58722b2a9fa0338048ae1e5c7e5989c9c562e209
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-05 18:50:50 +0300

    fix photo-album queries and mutations

---
+++ src/graphql/photo-album/index.js
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_PHOTO_ALBUM = gql`
+    mutation CreatePhotoAlbum($title: String!, $description: String!) {
+        createPhotoAlbum(title: $title, description: $description) {
+            success
+            message
+        }
+    }
+`;
+
+export const UPDATE_PHOTO_ALBUM = gql`
+    mutation UpdatePhotoAlbum(
+        $photoAlbumId: ID!
+        $title: String
+        $description: String
+    ) {
+        updatePhotoAlbum(
+            photoAlbumId: $photoAlbumId
+            title: $title
+            description: $description
+        )
+    }
+`;
+
+export const REMOVE_PHOTO_ALBUM = gql`
+    mutation RemovePhotoAlbum($photoAlbumId: ID!) {
+        removePhotoAlbum(photoAlbumId: $photoAlbumId) {
+            success
+            message
+        }
+    }
+`;
+
+export const GET_PHOTO_ALBUMS_BY_USER_ID = gql`
+    query GetPhotoAlbumsByUserId($userId: ID!) {
+        getPhotoAlbumsByUserId {
+            id
+            title
+            description
+        }
+    }
+`;
